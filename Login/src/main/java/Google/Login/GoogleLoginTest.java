5470a5de1335a4219f3de3fcaa23b89b241e38c8
dadvolod <dadvolodkin@Yahoo.com>
2016-07-18 14:20:07 -0400

    Updated Test

--- Login/src/main/java/Google/Login/GoogleLoginTest.java
+++ Login/src/main/java/Google/Login/GoogleLoginTest.java
@@ -1,13 +1,12 @@
 package Google.Login;
 
 import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+
 
 
 public class GoogleLoginTest {

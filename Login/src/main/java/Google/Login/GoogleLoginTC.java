80d881acb7103850688084efccf83644ddd170eb
dadvolod <dadvolodkin@Yahoo.com>
2016-07-18 14:38:25 -0400

    Take off annotation

--- Login/src/main/java/Google/Login/GoogleLoginTC.java
+++ Login/src/main/java/Google/Login/GoogleLoginTC.java
@@ -5,7 +5,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 import Google.Login.GLoginPage;
 
 public class GoogleLoginTC {

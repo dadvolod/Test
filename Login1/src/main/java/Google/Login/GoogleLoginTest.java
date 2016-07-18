b6110427a44f736a47add37b3f2bcc43c10b6d69
dadvolod <dadvolodkin@Yahoo.com>
2016-07-18 14:42:29 -0400

    Login1

---
+++ Login1/src/main/java/Google/Login/GoogleLoginTest.java
@@ -0,0 +1,38 @@
+package Google.Login;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+
+
+public class GoogleLoginTest {
+	
+	
+	public String gmailUsername = "gciotest@gmail.com";
+	public String gmailPassword = "Password123#";
+
+	
+	@Test
+	public void Login(){	
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/dmitriyadvolodkin/Documents/Google/Drivers/chromedriver");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		
+		driver.get("https://accounts.google.com/");
+		
+		GPage Glp = new GPage(driver);
+		Glp.GmailLogin(gmailUsername, gmailPassword);
+		
+		Assert.assertTrue(Glp.MyAccountText().isDisplayed(), "Login Passed");
+		
+		driver.quit();
+		
+	}
+
+}

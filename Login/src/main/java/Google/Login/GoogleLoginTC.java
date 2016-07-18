3d03b721b2f3c0700700ab4359c4633193520da3
dadvolod <dadvolodkin@Yahoo.com>
2016-07-18 14:40:12 -0400

    Updated Test

--- Login/src/main/java/Google/Login/GoogleLoginTC.java
+++ Login/src/main/java/Google/Login/GoogleLoginTC.java
@@ -4,7 +4,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import Google.Login.GLoginPage;
 
 public class GoogleLoginTC {
